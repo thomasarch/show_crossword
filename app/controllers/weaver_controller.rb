03ae8ce0021707713157b4c4b15d7ad0bbc22180
Thomas DeFelice <thomasarch@gmail.com>
2018-08-06 00:08:49 -0400

    disable creating log files

--- app/controllers/weaver_controller.rb
+++ app/controllers/weaver_controller.rb
@@ -246,7 +246,7 @@ class WeaverController < ApplicationController
 					@old_word[level] = @set_words[level]
 					@set_words[level] = @list[level].shift
 					puts "word #{level} #{@set_words}"
-					(level > 4) ? @answers.push(@set_words.values) : ''
+					(level >= 4) ? @answers.push(@set_words.values) : ''
 					find_word_diff(level)
 					search_with_ignore(level)
 					spread_with_ignore(level)
