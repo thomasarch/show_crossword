d360d2e90f9122e5571c97139db0213b7550622f
Haiz Oppenheimer <haizop@gmail.com>
2018-08-13 12:04:30 -0400

    annotate

--- app/models/split_word.rb
+++ app/models/split_word.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: split_words
+#
+#  id         :integer          not null, primary key
+#  word       :string
+#  l0         :string
+#  l1         :string
+#  l2         :string
+#  l3         :string
+#  l4         :string
+#  l5         :string
+#  l6         :string
+#  l7         :string
+#  l8         :string
+#  l9         :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class SplitWord < ApplicationRecord
 end
--- test/models/eight_letter_word_test.rb
+++ test/models/eight_letter_word_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: eight_letter_words
+#
+#  id         :integer          not null, primary key
+#  word       :string
+#  l0         :string
+#  l1         :string
+#  l2         :string
+#  l3         :string
+#  l4         :string
+#  l5         :string
+#  l6         :string
+#  l7         :string
+#  clue       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class EightLetterWordTest < ActiveSupport::TestCase
