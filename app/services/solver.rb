e683b7f14731cbaa82c035b43508a0f55ead2677
Thomas DeFelice <thomasarch@gmail.com>
2018-08-22 14:30:21 -0400

    removed the 'setup' methods cleaned up comments cleaed up other misc things

--- app/services/solver.rb
+++ app/services/solver.rb
@@ -11,55 +11,35 @@ class Solver
   end
 
   def call
-    # now = Time.now
     @answers = []
-
-    #
-    # This is where it all begins
-    # 	choose the first letter to test
-    #
     root_letter_words = @roots_words_hash[@root_letter].pluck(:word)
+    
     while root_letter_words.length > 1 && @answers.count < @answer_number
       @set_words = {}
       @set_words[:across0] = root_letter_words.shift
       @down_list = root_letter_words.clone
-
-      # sets up the inital hashes
       @searches = {}
       @spreads = {}
       @builders = {}
       @old_word = {}
-      @ignore = {}
+      @ignore = {0 => []}
       @list = {}
       @depth = 0
-
       old_down = nil
+      
       while @down_list.count > 0 && @answers.count < @answer_number
         @down = @down_list.shift
         @set_words[:down0] = @down
-
-        setup_search
-
-        setup_spreads(0)
-        setup_builders(0)
+        search(0)
+        spread(0)
+        builders(0)
         @list[1] = create_new_word_list(0)
-
-        search_block(1)
-
+        unless @list[1].length == 0
+          search_block(1)
+        end
         prune_list
-        hold = @set_words[:across0]
-        @set_words = {}
-        @set_words[:across0] = hold
-
-        # end level 0 pick new down word
       end
-
-      # pick new root word
     end
-
-    # later = Time.now
-    # howlong = "took #{later - now} seconds"
-    # @answers.push(howlong)
 
     file_name = File.join(File.dirname(__FILE__), "../../lib/answers/five_letter_answers/#{@root_letter}.yml")
 
@@ -82,29 +62,30 @@ class Solver
     end.to_h
   end
 
-  def setup_search
+  def search(level)
     array = []
-    @set_words[:across0].split('').each_with_index do |letter, i|
-      unless i == 0
-        array[i] = @roots_words_hash[letter]
+    if level == 0
+      @set_words[:across0].split('').each_with_index do |letter, i|
+        unless i == 0
+          array[i] = @roots_words_hash[letter]
+        end
+      end
+      @searches[0] = array
+    else
+      unless @searches[level] == nil
+        array = @searches[level].clone
+      end
+      @set_words[level].split('').each_with_index do |letter, i|
+        unless i == 0 || @ignore[level][i] == true
+          array[i] = eval("@searches[level - 1][i].where(l#{level}:letter)")
+        end
       end
     end
-    @searches[0] = array
+    @searches[level] = array
   end
 
-  # set up the spreads
-  def setup_spreads(level)
-    array = []
-    @searches[level].each_with_index do |record, i|
-      unless i == 0
-        array[i] = eval("record.pluck(:l#{level + 1}).uniq")
-      end
-    end
-    @spreads[level] = array
-  end
-
-  def spread_with_ignore(level)
-    if @spreads[level - 1] == nil
+  def spread(level)
+    if @spreads[level - 1] == nil || level == 0
       array = []
     else
       array = @spreads[level - 1].clone
@@ -117,44 +98,35 @@ class Solver
     @spreads[level] = array
   end
 
-  # set up the builder list
-  def setup_builders(level)
-    array = []
-    @spreads[level].each_with_index do |letters, i|
-      if i == 0
-        array[i] = @down[level + 1]
-      else
-        array[i] = @deep_followers[@down[level + 1]][i] & letters
-      end
-    end
-    @builders[level] = array
-  end
-
-  def builders_with_ignore(level)
-    if @builders[level] == nil
+  def builders(level)
+    if level == 0
       array = []
+      @spreads[level].each_with_index do |letters, i|
+        if i == 0
+          array[i] = @down[level + 1]
+        else
+          array[i] = @deep_followers[@down[level + 1]][i] & letters
+        end
+      end
     else
-      array = @builders[level].clone
-    end
-
-    # 3.times {puts ""}
-    # puts "builder array before ignore"
-    # p array
-    # p @ignore[level]
-    @spreads[level].each_with_index do |letters, i|
-      if i == 0
-        array[i] = @down[level + 1]
-      elsif @ignore[level][i] == true
+      if @builders[level] == nil
+        array = []
       else
-        array[i] = @deep_followers[@down[level + 1]][i] & letters
+        array = @builders[level].clone
+      end
+
+      @spreads[level].each_with_index do |letters, i|
+        if i == 0
+          array[i] = @down[level + 1]
+        elsif @ignore[level][i] == true
+        else
+          array[i] = @deep_followers[@down[level + 1]][i] & letters
+        end
       end
     end
     @builders[level] = array
-    # puts "builder after ignore"
-    # p @builders[level]
   end
 
-  # use the builders to create a list of next possible words
   def create_new_word_list(level)
     builders = @builders[level]
     @words.where(
@@ -164,30 +136,6 @@ class Solver
       l3: builders[3],
       l4: builders[4]
     ).where.not(word: @set_words.values).pluck(:word)
-  end
-
-  def refine_search(level)
-    array = []
-    @set_words[level].split('').each_with_index do |letter, i|
-      unless i == 0
-        array[i] = eval("@searches[level - 1][i].where(l#{level}:letter)")
-      end
-    end
-    @searches[level] = array
-  end
-
-  def search_with_ignore(level)
-    if @searches[level] == nil
-      array = []
-    else
-      array = @searches[level].clone
-    end
-    @set_words[level].split('').each_with_index do |letter, i|
-      unless i == 0 || @ignore[level][i] == true
-        array[i] = eval("@searches[level - 1][i].where(l#{level}:letter)")
-      end
-    end
-    @searches[level] = array
   end
 
   def find_word_diff(level)
@@ -205,16 +153,8 @@ class Solver
   end
 
   def prune_list
-    # puts "down word: #{@down}"
-    # puts "depth: #{@depth}"
-    # puts "before #{@down_list}"
-
     @down_list.select! {|word| word[0..(@depth + 1)] != @down[0..(@depth + 1)] }
     @depth = 0
-
-    # end
-    # puts ""
-    # puts "after #{@down_list}"
   end
 
   def search_block(level)
@@ -225,9 +165,9 @@ class Solver
         @set_words[level] = @list[level].shift
         puts "word #{level} #{@set_words}"
         find_word_diff(level)
-        search_with_ignore(level)
-        spread_with_ignore(level)
-        builders_with_ignore(level)
+        search(level)
+        spread(level)
+        builders(level)
         @list[level + 1] = create_new_word_list(level)
         set_depth(level)
         if @list[level + 1].count > 0 && level < 4
