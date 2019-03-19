9dc8763812f8ffe0f10984cdf3ad1ca0fa3cadd3
Thomas DeFelice <thomasarch@gmail.com>
2019-03-19 00:28:45 -0400

    cleaned up random_solver_spec file

--- spec/services/random_solver_spec.rb
+++ spec/services/random_solver_spec.rb
@@ -1,26 +1,25 @@
-require "rails_helper"
-require "json"
+require 'rails_helper'
+require 'json'
 
 RSpec.describe Solver do
- 
-  let(:params) { { letter: "h", number: "r"} }
+  let(:params) { { letter: 'h', number: 'r' } }
 
-  it 'checks that all the words contained in a randomly generated square are valid words' do
+  it 'validates words in a randomly generated square' do
     answer = Solver.new(params).call[0][0]
+    binding.irb
     answer.delete_at(1)
 
     down_list = {}
     answer[0].split('').each_with_index do |letter, i|
       down_list[i] = letter
-      4.times do |count| 
+      4.times do |count|
         count += 1
         down_list[i].concat(answer[count][i])
       end
     end
 
-    down_list.each do |k, v|
+    down_list.each do |_k, v|
       expect(FiveLetterWord.where(word: v).count).to eq(1)
     end
-
   end
 end
